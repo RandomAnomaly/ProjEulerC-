9b8cbacafdefa7cb19929b0976d33272ea85884f
jack <jack.gammon@equinox.co.nz>
2016-12-20 11:29:28 +1300

    Completed problem 2

--- Project Euler/Even Fibonacci/Program.cs
+++ Project Euler/Even Fibonacci/Program.cs
@@ -10,6 +10,44 @@ namespace Even_Fibonacci
     {
         static void Main(string[] args)
         {
+            int a = 1;
+            int b = 1;
+            int total = 0;
+            Console.WriteLine(1);
+            Console.WriteLine(1);
+            while(a + b < 4000000)
+            {
+                int newFib = a + b;
+                
+                if(b >= a)
+                {
+                    a = newFib;
+                } else
+                {
+                    b = newFib;
+                }
+                if(newFib % 2 == 0)
+                {
+                    Console.WriteLine(newFib + "\t\t\tEven");
+                    total += newFib;
+                } else
+                {
+                    Console.WriteLine(newFib);
+                }
+                
+            }
+
+            Console.WriteLine("\n\n\tSum of even Fibonacci below 4,000,000: " + total);
+
+            new Program().delayClose();
+        }
+
+        
+
+        void delayClose()
+        {
+            Console.Write("Press enter to close...");
+            Console.ReadLine();
         }
     }
 }
---
+++ Project Euler/Helpers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    class Program
+    {
+        
+        void delayClose()
+        {
+            Console.Write("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
